9d9541b47ae9dbea9ce5518c2d2cd2f886f596cd
Xtron [@pro_noober] <84863955+okaybro526-1@users.noreply.github.com>
2022-07-06 18:37:48 +0530

    Create text.py

---
+++ bot/modules/text.py
@@ -0,0 +1,41 @@
+from pyrogram.emoji import *
+
+
+class TEXT:
+
+    ABOUT = """
+
+<b>------About This Bot------</b>
+
+Soon I'll write something here till then enjoy 
+
+ - @pro_noober
+
+My Owner :- <a href='t.me/pro_noober'>Xtron</a>
+
+"""
+
+    HELP_USER = """**Follow Below Steps:**
+   
+- Use /mode command to change upload mode.
+- Send a photo to set as permanent thumbnail.
+- Now send me the Telegram file you want to rename.
+- Send the new name when bot ask.
+
+**To Increce Speed**
+
+- Use Normal Font Style As File Name
+- After File Name Give File category(Mkv/Mp4/..etc)
+- Wait Untill Previous Process succsus
+"""
+
+    START_TEXT = """Hi {user_mention},
+
+I am a telegram renamer Bot. Use /help For more details...
+"""
+
+    UPDATES = """
+
+<b>Updates Channel :</b> <a href='t.me/xtronbots'>Xtron Bots 🤖</a>
+
+"""
