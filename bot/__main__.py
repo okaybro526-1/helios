a9b0324b2327dce470d63e94c78544e2b5a28ef4
Xtron [@pro_noober] <84863955+okaybro526-1@users.noreply.github.com>
2022-09-14 03:56:40 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -85,7 +85,7 @@ def ping(update, context):
     start_time = int(round(time() * 1000))
     reply = sendMessage("Starting Ping", context.bot, update.message)
     end_time = int(round(time() * 1000))
-    editMessage(f'**🏓Ping**- {end_time - start_time} ms', reply)
+    editMessage(f'<b>🏓Ping</b>- {end_time - start_time} ms', reply)
 
 
 def log(update, context):
